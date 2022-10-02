22651a0911063083be2cfa41d1172fea3fff7d6a
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-02 15:48:14 +1100

    commit tree ef2c219b80107b8cb5c0e2c47172c70f38fecd99

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener ('DOMContentLoaded', () => {
+    document.querySelector('p').innerHTML = "This is really cool!";
+})
+
+
+
+document.querySelector('p').innerText = "This is really cool!";
